fd7e8464dd9ac1f9520dc48f9f7dc8366e1e6c75
sankar-maker <65747675+sankar-maker@users.noreply.github.com>
2021-11-02 01:43:18 +0530

    Add files via upload

---
+++ paystubapp/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from     paystubapp.models import Paystub
+# Register your models here.
+
+admin.site.register(Paystub)
---
+++ paystubapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PaystubappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'paystubapp'
